7d7f051dac5249249ceee7ca5b82a98f486f223f
Marek Vondra <marek.vondra@t-systems.com>
2024-11-19 13:38:30 +0100

    maven plugin for generate opeAPi, full information about swagger

--- src/main/java/de/engeto/springbootindocker/configuration/OpeApiConfiguration.java
+++ src/main/java/de/engeto/springbootindocker/configuration/OpeApiConfiguration.java
@@ -1,10 +1,16 @@
 package de.engeto.springbootindocker.configuration;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.servers.ServerVariable;
 import io.swagger.v3.oas.models.servers.ServerVariables;
+import io.swagger.v3.oas.models.tags.Tag;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,33 +22,39 @@ public class OpeApiConfiguration {
     @Bean
     public OpenAPI customOpenApi(){
         return new OpenAPI()
+                .components(new Components())
                 .info(new Info()
-                        .title("AIS - Book manager")
-                        .description("This is the swagger for the book")
-                )
-                .servers(
-                        List.of(
-                             new Server()
-                                     .url("localhost.cz")
-                                     .description("Develop server")
-                            ,
-                            new Server()
-                                    .url("{protocol}://sas.prota.com:{port}")
-                                    .variables(
-                                            new ServerVariables()
-                                                    .addServerVariable("protocol",
-                                                            new ServerVariable()
-                                                                    ._default("https")
-                                                                    .addEnumItem("https")
-                                                                    .addEnumItem("http"))
-                                                    .addServerVariable("port",
-                                                            new ServerVariable()
-                                                                    ._default("8080")
-                                                                    .description("Port on which is the application running"))
-                                    )
-                        )
-
-                );
+                        .title("AIS ")
+                        .description("This is try project")
+                        .version("1.0.0")
+                        .license(new License().name("Apache 2.0").url("http://www.apache.org/licenses/LICENSE-2.0"))
+                        .contact(new Contact().name("API Support").url("http://example.com/contact").email("support@example.com")))
+                .servers(List.of(
+                        new Server().url("{protocol}://{customerId}.saas-app.com:{port}/v2").description("Test server")
+                                .variables(new ServerVariables()
+                                        .addServerVariable("customerId",
+                                                new ServerVariable()
+                                                        ._default("demo")
+                                                        .description("Customer ID assigned by the service provider"))
+                                        .addServerVariable("protocol",
+                                                new ServerVariable()
+                                                        ._default("https")
+                                                        .addEnumItem("http")
+                                                        .addEnumItem("https"))
+                                        .addServerVariable("port",
+                                                new ServerVariable()
+                                                        ._default("443")
+                                                        .addEnumItem("443")
+                                                        .addEnumItem("8443")))   ,
+                        new Server().url("https://production.com").description("Production Server")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Find more info here")
+                        .url("http://example.com/docs"))
+                .components(new Components().addSecuritySchemes("bearerAuth", new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")))
+                .tags(List.of(new Tag().name("Authentication").description("Operations related to authentication")));
     }
 
 }
